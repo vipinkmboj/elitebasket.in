58796b169a00ddc670755724e699749b6e697f38
Vipin Kumar <vipinkmboj21@gmail.com>
2022-03-25 12:54:49 +0530

    Some Features Created

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Elite Basket' });
+  res.render('index', { title: 'Elite Basket', msg: '' });
 });
 
 module.exports = router;
